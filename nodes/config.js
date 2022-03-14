95fdb02ad9b68c2c30dcde213ad0e0af08ae867c
Simone Ballard <simone.a.ballard@gmail.com>
2022-03-14 14:12:19 -0500

    as far as we get with what we have

--- nodes/config.js
+++ nodes/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    access_token: PAN_SEC_TWIT_API_KEY,
-    access_token_secret: PAN_SEC_TWIT_API_SECRET_KEY
+    access_token = process.env.PAN_SEC_TWIT_API_KEY,
+    access_token_secret = process.env.PAN_SEC_TWIT_API_SECRET_KEY
 }
